0824acf03a7ed27a813ac2160ebb5a6193a6df03
magixnetworks <89393965+magixnetworks@users.noreply.github.com>
2025-01-24 14:03:41 +1100

    Adjustments for Alpine directories, removed VirtualHost as it's not necessary here

--- alpine/entrypoint.sh
+++ alpine/entrypoint.sh
@@ -1,55 +1,42 @@
 #!/bin/bash
-# Update the apache2 sites-available
-echo "<Directory '/var/www/html'>
-   Order allow,deny
-   Allow from all
-   Require all granted
-</Directory>
-<VirtualHost *:$ITFLOW_PORT>
-    ServerName $ITFLOW_URL
-    DocumentRoot /var/www/html/
-    LogLevel $ITFLOW_LOG_LEVEL
-    ErrorLog /var/log/apache2/error.log
-    CustomLog /var/log/apache2/access.log combined
-</VirtualHost>" > /etc/apache2/sites-available/000-default.conf
 
-sed -i "s/^Listen.*/Listen $ITFLOW_PORT/g" /etc/apache2/ports.conf
+sed -i "s/^Listen.*/Listen $ITFLOW_PORT/g" /etc/apache2/httpd.conf
 
 # if itflow is not downloaded, perform the download after the volume mounting process within dockerfile is complete.
-if [[ -f /var/www/html/index.php ]]; then 
-    cd /var/www/html
+if [[ -f /var/www/localhost/htdocs/index.php ]]; then 
+    cd /var/www/localhost/htdocs
     # git fetch
 else
-    git clone --branch $ITFLOW_REPO_BRANCH https://$ITFLOW_REPO /var/www/html
+    git clone --branch $ITFLOW_REPO_BRANCH https://$ITFLOW_REPO /var/www/localhost/htdocs
 fi
 
-git config --global --add safe.directory /var/www/html
+git config --global --add safe.directory /var/www/localhost/htdocs
 
 # Verify permissions of itflow git repository
-chown -R www-data:www-data /var/www/html
+chown -R apache:apache /var/www/localhost/htdocs
 
 # This updates the config.php file once initialization through setup.php has completed
-if [[ -f /var/www/html/config.php ]]; then 
+if [[ -f /var/www/localhost/htdocs/config.php ]]; then 
     # Company Name
-    sed -i "s/\$config_app_name.*';/\$config_app_name = '$ITFLOW_NAME';/g" /var/www/html/config.php
+    sed -i "s/\$config_app_name.*';/\$config_app_name = '$ITFLOW_NAME';/g" /var/www/localhost/htdocs/config.php
 
     # MariaDB Host
-    sed -i "s/\$dbhost.*';/\$dbhost = '$ITFLOW_DB_HOST';/g" /var/www/html/config.php
+    sed -i "s/\$dbhost.*';/\$dbhost = '$ITFLOW_DB_HOST';/g" /var/www/localhost/htdocs/config.php
 
     # Database Password
-    sed -i "s/\$dbpassword.*';/\$dbpassword = '$ITFLOW_DB_PASS';/g" /var/www/html/config.php
+    sed -i "s/\$dbpassword.*';/\$dbpassword = '$ITFLOW_DB_PASS';/g" /var/www/localhost/htdocs/config.php
 
     # Base URL
-    sed -i "s/\$config_base_url.*';/\$config_base_url = '$ITFLOW_URL';/g" /var/www/html/config.php
+    sed -i "s/\$config_base_url.*';/\$config_base_url = '$ITFLOW_URL';/g" /var/www/localhost/htdocs/config.php
 
     # Repo Branch
-    sed -i "s/\$repo_branch.*';/\$repo_branch = '$ITFLOW_REPO_BRANCH';/g" /var/www/html/config.php
+    sed -i "s/\$repo_branch.*';/\$repo_branch = '$ITFLOW_REPO_BRANCH';/g" /var/www/localhost/htdocs/config.php
     
-    find /var/www/html -type d -exec chmod 775 {} \;
-    find /var/www/html -type f -exec chmod 664 {} \;
-    chmod 640 /var/www/html/config.php
+    find /var/www/localhost/htdocs -type d -exec chmod 775 {} \;
+    find /var/www/localhost/htdocs -type f -exec chmod 664 {} \;
+    chmod 640 /var/www/localhost/htdocs/config.php
 else 
-    chmod -R 777 /var/www/html
+    chmod -R 777 /var/www/localhost/htdocs
 fi
 
 # Enable the apache2 sites-available
